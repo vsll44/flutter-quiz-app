c261e0f0b6a45531495289a5e913e5f76c1b74d6
Sebuhi Huseynli <sebuhihuseyinli8@gmail.com>
2025-05-18 19:30:16 +0400

    Add quiz model

---
+++ lib/models/quiz_model.dart
@@ -0,0 +1,38 @@
+class Quiz {
+  final String title;
+  final List<Question> questions;
+
+  Quiz({required this.title, required this.questions});
+
+  factory Quiz.fromJson(Map<String, dynamic> json) {
+    return Quiz(
+      title: json['title'],
+      questions: (json['questions'] as List)
+          .map((q) => Question.fromJson(q))
+          .toList(),
+    );
+  }
+}
+
+class Question {
+  final String question;
+  final String? imageUrl;
+  final List<String> options;
+  final int correctIndex;
+
+  Question({
+    required this.question,
+    this.imageUrl,
+    required this.options,
+    required this.correctIndex,
+  });
+
+  factory Question.fromJson(Map<String, dynamic> json) {
+    return Question(
+      question: json['question'],
+      imageUrl: json['imageUrl'],
+      options: List<String>.from(json['options']),
+      correctIndex: json['correctIndex'],
+    );
+  }
+}
